f337a6f60720de74f757b4fb356ee86ea5d08391
AntonyCheng <1911261716@qq.com>
2024-05-03 20:49:54 +0800

    SpringBoot初始化模板v2.1.5 开发Java17+SpringBoot3升级模块

--- upgrade/spring-boot-init-template-upgrade/src/test/java/top/sharehome/springbootinittemplate/ai/ollama/AiOllamaTest.java
+++ upgrade/spring-boot-init-template-upgrade/src/test/java/top/sharehome/springbootinittemplate/ai/ollama/AiOllamaTest.java
@@ -1,32 +1,32 @@
-package top.sharehome.springbootinittemplate.ai.ollama;
-
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.Test;
-import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-/**
- * 切点参数为execution型的切面测试类
- *
- * @author AntonyCheng
- */
-@SpringBootTest
-public class AiOllamaTest {
-
-    @Resource
-    private OllamaChatClient ollamaChatClient;
-
-    @Test
-    public void testAiOllama(){
-        ChatResponse call = ollamaChatClient.call(new Prompt(List.of(new SystemMessage("你是一个非常伟大的计算机科学家，名字叫做AntonyCheng"), new UserMessage("你的名字叫什么"))));
-        System.out.println(call.toString());
-    }
-
-}
+//package top.sharehome.springbootinittemplate.ai.ollama;
+//
+//import jakarta.annotation.Resource;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.ai.chat.ChatResponse;
+//import org.springframework.ai.chat.messages.Message;
+//import org.springframework.ai.chat.messages.SystemMessage;
+//import org.springframework.ai.chat.messages.UserMessage;
+//import org.springframework.ai.chat.prompt.Prompt;
+//import org.springframework.ai.ollama.OllamaChatClient;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+///**
+// * 切点参数为execution型的切面测试类
+// *
+// * @author AntonyCheng
+// */
+//@SpringBootTest
+//public class AiOllamaTest {
+//
+//    @Resource
+//    private OllamaChatClient ollamaChatClient;
+//
+//    @Test
+//    public void testAiOllama(){
+//        ChatResponse call = ollamaChatClient.call(new Prompt(List.of(new SystemMessage("你是一个非常伟大的计算机科学家，名字叫做AntonyCheng"), new UserMessage("你的名字叫什么"))));
+//        System.out.println(call.toString());
+//    }
+//
+//}
